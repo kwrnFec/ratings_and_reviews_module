7f06b527f90a596849203a43bd53f58ca9e0c684
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-09 10:49:46 -0500

    Fixed Jest config issue with CSS modules, Fixed errors with tests looking for wrong elements

--- client/src/components/Review/Review.test.js
+++ client/src/components/Review/Review.test.js
@@ -5,25 +5,25 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-describe('review component should have 5 children', () => {
+describe('review component should have 4 children', () => {
   test('renders a component', () => {
     let wrapper = shallow(<Review />);
-    expect(wrapper.find('#review-container').children()).toHaveLength(5);
+    expect(wrapper.find('.reviewContainer').children()).toHaveLength(4);
   })
   
   test('should have a stars component', () => {
     let wrapper = shallow(<Review />);
-    expect(wrapper.find('#stars-placeholder').exists()).toBe(true);
+    expect(wrapper.find('.starsPlaceholder').exists()).toBe(true);
   })
   
-  test('should have a title element', () => {
+  test('should have a title', () => {
     let wrapper = shallow(<Review />);
-    expect(wrapper.find('#title').exists()).toBe(true);
+    expect(wrapper.find('h3').exists()).toBe(true);
   })
   
-  test('should have a review-body element', () => {
+  test('should have a review body paragraph', () => {
     let wrapper = shallow(<Review />);
-    expect(wrapper.find('#review-body').exists()).toBe(true);
+    expect(wrapper.find('p').exists()).toBe(true);
   })
   
 })
