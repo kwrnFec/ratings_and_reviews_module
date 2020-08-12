6665781adb03a36dd5a560724575af484d618355
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-11 21:24:02 -0500

    Added Moment library to format time/date. Fixed minor styling issues

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -7,33 +7,33 @@ import style from './app.css';
 
 const App = () => {
   const [data, setData] = useState([]);
-  
+
   // Api call to get data
   useEffect(() => {
     let param = 24;
-    
+
     const getData = async () => {
       let res = await fetch(`/reviews/${param}/list`);
       let data = await res.json();
       return data;
     }
-    
+
     getData()
-    .then(data => setData(data.results));
+      .then(data => setData(data.results));
     // [] argument below ensures this only occurs on mount not on update
   }, [])
 
   return (
-  <Container className={style.container}>
-    <Row>
-      <Col>
-        <div xs={12} lg={4} className={style.ratingPlaceholder}></div>
-      </Col>
-      <Col xs={12} lg={8}>
-        {data.length > 0 ? data.map(el => <Review data={ el } key={ el.review_id } />) : ''}
-      </Col>
-    </Row>
-  </Container>
+    <Container className={style.container}>
+      <Row>
+        <Col xs={12} lg={4}>
+          <div className={style.ratingPlaceholder}></div>
+        </Col>
+        <Col xs={12} lg={8}>
+          {data.length > 0 ? data.map(el => <Review data={el} key={el.review_id} />) : ''}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
