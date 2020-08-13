2fada9c977f0bb89f516d671cb9cba9c2b7889e3
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-13 16:40:16 -0500

    Extracted rating bars into seperate component, made overall average rating functional

--- client/src/components/Summary/Summary.jsx
+++ client/src/components/Summary/Summary.jsx
@@ -1,70 +1,32 @@
-import React from 'react';
+/* eslint-disable import/extensions */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
 import style from './summary.css';
 
+import RatingBars from '../RatingBars/RatingBars.jsx';
+
 const Summary = (props) => {
   const { data } = props;
+  const [avgRating, setAvgRating] = useState('-');
 
-  // Calculates the width of the inner bar to fill the rating bars
-  const calculateRatingBars = (ratingNum) => {
-    // if (data !== undefined) {
-    let sortedData = [];
-    let totalSame = 0;
-    let totalReviews = 0;
-    let percent = 0;
+  // Calculates average rating
+  const averageRating = () => {
+    let total = 0;
 
-    // iterate over data and push each reviews rating to a new array
-    if (data !== undefined) {
-      data.forEach((el) => {
-        sortedData.push(el.rating);
-      });
+    data.forEach((el) => {
+      total += el.rating;
+    });
 
-      // sort and reverse the array so the data will line up
-      // with the order the bars are rendered in
-      sortedData = sortedData.sort().reverse();
-    }
-
-    // iterate the data array, add up total reviews and total of the matching ratings
-    for (let i = 0; i < sortedData.length; i += 1) {
-      totalReviews += 1;
-      if (sortedData[i] === ratingNum) {
-        totalSame += 1;
-      }
-    }
-
-    // if no reviews of a number exist return 0
-    if (totalSame === 0) {
-      return 0;
-    }
-
-    // calculate percent
-    percent = totalSame / totalReviews;
-    return percent.toFixed(1) * 100;
+    return (total / data.length).toFixed(1);
   };
 
-  // Generate 5 rating bars
-  const createRatingBars = () => {
-    const elements = [];
-    let rating = 0;
-
-    for (let i = 5; i > 0; i -= 1) {
-      // calculate percentage to fill rating bar
-      rating = calculateRatingBars(i);
-
-      elements.push(
-        <Row className={style.flexRatingBarRow} key={i}>
-          <Col xs={3}>
-            <span className={style.spanFont}>{`${i} Stars`}</span>
-          </Col>
-          <Col xs={8} className={style.outerBar}>
-            <div className={style.innerBar} style={{ width: `${rating}%`, transition: '2s' }} />
-          </Col>
-        </Row>,
-      );
+  // Runs average Rating function to generate number to display
+  useEffect(() => {
+    if (data.length > 0) {
+      setAvgRating(averageRating());
     }
-    return elements;
-  };
+  });
 
   return (
     <Container className={style.ratingPlaceholder}>
@@ -75,7 +37,7 @@ const Summary = (props) => {
       </Row>
       <Row>
         <Col xs={6} lg={5}>
-          <span className={style.rating}>3.5</span>
+          <span className={style.rating}>{ `${avgRating}` }</span>
         </Col>
         <Col xs={5} className={style.starsPlaceholder}>Stars</Col>
       </Row>
@@ -84,7 +46,7 @@ const Summary = (props) => {
           <span className={style.spanFont}>100% of reviews recommend this product</span>
         </Col>
       </Row>
-      {createRatingBars()}
+      <RatingBars data={data} />
     </Container>
   );
 };
