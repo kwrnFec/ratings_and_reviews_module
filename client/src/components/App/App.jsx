8a608634f86cd1c70bfe6485a757a0d0b327bd86
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-13 13:07:13 -0500

    Fixed all current ESLint issues

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
 import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line object-curly-newline
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import regeneratorRuntime from 'regenerator-runtime';
 import axios from 'axios';
 
+// COMPONENTS
 import Review from '../Review/Review.jsx';
 import Summary from '../Summary/Summary.jsx';
 import style from './app.css';
@@ -13,43 +17,46 @@ const App = () => {
 
   // Api call to get data
   useEffect(() => {
-    let param = 26;
+    const param = 24;
 
     const getData = async () => {
       axios.get(`/reviews/${param}/list`)
         .then((response) => {
-          setData(response.data.results)
-        })
-    }
-    getData()
+          setData(response.data.results);
+        });
+    };
+    getData();
     // [] argument below ensures this only occurs on mount not on update
-  }, [])
+  }, []);
 
+  // Creates the list of review tiles to be rendered
   const renderReviews = () => {
     const reviews = [];
 
-    for (let i = 0; i < renderCount; i++) {
+    for (let i = 0; i < renderCount; i += 1) {
       if (data[i] !== undefined) {
-        reviews.push(<Review data={data[i]} key={data[i].review_id} />)
+        reviews.push(<Review data={data[i]} key={data[i].review_id} />);
       }
     }
     return reviews;
-  }
+  };
 
+  // Gets 2 more reviews when button is clicked
   const getMoreReviews = () => {
     setRenderCount(renderCount + 2);
     renderReviews();
-  }
+  };
 
+  // Controls conditional rendering of more reviews button
   const getMoreReviewsButton = () => {
     if (data.length < 2) {
-      return;
-    } else if (data.length === renderCount || renderCount > data.length) {
-      return;
-    } else if (data.length > 2) {
-      return <Button id='moreReviews' className='mr-3 mt-4' variant='outline-dark' onClick={getMoreReviews}>More Reviews</Button>
+      return null;
     }
-  }
+    if (data.length === renderCount || renderCount > data.length) {
+      return null;
+    }
+    return <Button id="moreReviews" className="mr-3 mt-4" variant="outline-dark" onClick={getMoreReviews}>More Reviews</Button>;
+  };
 
   return (
     <Container>
@@ -57,14 +64,14 @@ const App = () => {
         <Col xs={10} md={6} lg={4}>
           <Summary data={data} />
         </Col>
-        <Col xs={12} lg={8} id='reviewsContainer' className={style.container}>
+        <Col xs={12} lg={8} id="reviewsContainer" className={style.container}>
           {renderReviews()}
           {getMoreReviewsButton()}
-          <Button className='mt-4' variant='outline-dark'>Add A Review &#43;</Button>
+          <Button className="mt-4" variant="outline-dark">Add A Review &#43;</Button>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default App;
--- client/src/components/Summary/Summary.jsx
+++ client/src/components/Summary/Summary.jsx
@@ -1,67 +1,70 @@
 import React from 'react';
-import style from './summary.css';
+import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
+import style from './summary.css';
 
 const Summary = (props) => {
+  const { data } = props;
 
+  // Calculates the width of the inner bar to fill the rating bars
   const calculateRatingBars = (ratingNum) => {
-    if (props.data !== undefined) {
-      let data = [];
-      let totalSame = 0;
-      let totalReviews = 0;
-      let percent = 0;
+    // if (data !== undefined) {
+    let sortedData = [];
+    let totalSame = 0;
+    let totalReviews = 0;
+    let percent = 0;
 
-      // iterate over data and push each reviews rating to a new array
-      if (props.data !== undefined) {
-        props.data.forEach(el => {
-          data.push(el.rating);
-        })
+    // iterate over data and push each reviews rating to a new array
+    if (data !== undefined) {
+      data.forEach((el) => {
+        sortedData.push(el.rating);
+      });
 
-        // sort and reverse the array so the data will line up 
-        // with the order the bars are rendered in
-        data = data.sort().reverse();
-      }
-
-      // iterate the data array, add up total reviews and total of the matching ratings
-      for (let i = 0; i < data.length; i++) {
-        totalReviews += 1;
-        if (data[i] === ratingNum) {
-          totalSame += 1;
-        }
-      }
-
-      // if no reviews of a number exist return 0 
-      if (totalSame === 0) {
-        return 0;
-      }
-
-      // calculate percent, return as a 
-      percent = totalSame / totalReviews;
-      return percent.toFixed(1) * 100;
+      // sort and reverse the array so the data will line up
+      // with the order the bars are rendered in
+      sortedData = sortedData.sort().reverse();
     }
-  }
 
+    // iterate the data array, add up total reviews and total of the matching ratings
+    for (let i = 0; i < sortedData.length; i += 1) {
+      totalReviews += 1;
+      if (data[i] === ratingNum) {
+        totalSame += 1;
+      }
+    }
+
+    // if no reviews of a number exist return 0
+    if (totalSame === 0) {
+      return 0;
+    }
+
+    // calculate percent
+    percent = totalSame / totalReviews;
+    return percent.toFixed(1) * 100;
+  };
+
+  // Generate 5 rating bars
   const createRatingBars = () => {
     const elements = [];
     let rating = 0;
 
-    for (let i = 5; i > 0; i--) {
+    for (let i = 5; i > 0; i -= 1) {
       // calculate percentage to fill rating bar
-      rating = calculateRatingBars(i)
+      rating = calculateRatingBars(i);
 
       elements.push(
         <Row className={style.flexRatingBarRow} key={i}>
           <Col xs={3}>
-            <span className={style.spanFont}>{i} Stars</span>
+            <span className={style.spanFont}>{`${i} Stars`}</span>
           </Col>
           <Col xs={8} className={style.outerBar}>
-            <div className={style.innerBar} style={{ width: `${rating}%`, transition: '2s' }}></div>
+            <div className={style.innerBar} style={{ width: `${rating}%`, transition: '2s' }} />
           </Col>
-        </Row>
-      )
+        </Row>,
+      );
     }
     return elements;
-  }
+  };
 
   return (
     <Container className={style.ratingPlaceholder}>
@@ -83,7 +86,15 @@ const Summary = (props) => {
       </Row>
       {createRatingBars()}
     </Container>
-  )
-}
+  );
+};
+
+Summary.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+};
+
+Summary.defaultProps = {
+  data: [{ body: 'default' }],
+};
 
 export default Summary;
