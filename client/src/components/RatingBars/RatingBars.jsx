d5997de41fb482038f5aec3440764ae0a844d9e2
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-14 13:34:11 -0500

    Removed no longer needed code block that was commented out, it was moved to helpers file

--- client/src/components/RatingBars/RatingBars.jsx
+++ client/src/components/RatingBars/RatingBars.jsx
@@ -8,43 +8,6 @@ import { calculateRatingBars } from '../../helpers/helpers.js';
 const RatingBars = (props) => {
   const { data } = props;
 
-  // // Calculates the width of the inner bar to fill the rating bars
-  // const calculateRatingBars = (ratingNum) => {
-  //   // if (data !== undefined) {
-  //   let sortedData = [];
-  //   let totalSame = 0;
-  //   let totalReviews = 0;
-  //   let percent = 0;
-
-  //   // iterate over data and push each reviews rating to a new array
-  //   if (data !== undefined) {
-  //     data.forEach((el) => {
-  //       sortedData.push(el.rating);
-  //     });
-
-  //     // sort and reverse the array so the data will line up
-  //     // with the order the bars are rendered in
-  //     sortedData = sortedData.sort().reverse();
-  //   }
-
-  //   // iterate the data array, add up total reviews and total of the matching ratings
-  //   for (let i = 0; i < sortedData.length; i += 1) {
-  //     totalReviews += 1;
-  //     if (sortedData[i] === ratingNum) {
-  //       totalSame += 1;
-  //     }
-  //   }
-
-  //   // if no reviews of a number exist return 0
-  //   if (totalSame === 0) {
-  //     return 0;
-  //   }
-
-  //   // calculate percent
-  //   percent = totalSame / totalReviews;
-  //   return percent.toFixed(1) * 100;
-  // };
-
   // Generate 5 rating bars
   const createRatingBars = () => {
     const elements = [];
